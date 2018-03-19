5da003687e48d61c714d59d89d5b00d22d143c6c
Isaac Mohammed Newton <theisaacnewtongh@gmail.com>
2018-03-19 12:45:58 +0000

    Bukaris work

---
+++ myjs/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#min").click(function(){
+    $("#ministiedd").toggle("slow");
+  });
+  $("#mins").click(function(){
+    $("#bookshopdb").toggle("slow");
+  });
+});
